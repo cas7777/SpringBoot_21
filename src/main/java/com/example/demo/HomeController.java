4632357585452f55d8c5888ed36912cd27a2f032
GBTC441008ur <claudeswanson2005@yahoo.com>
2018-03-16 12:22:08 -0400

    Project 20

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,15 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-
         return "index";
     }
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
-    @RequestMapping("/admin")
-    public String admin(){
-        return "admin";
+    @RequestMapping("/secure")
+    public String secure(){
+        return "secure";
     }
 }
